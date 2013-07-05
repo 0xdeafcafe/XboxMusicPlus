e1c00afe88caadb3651a90f18704a50a0d541ee3
Alex Reed (Xerax) <alexreed@outlook.com>
2013-07-05 22:59:32 +0100

    created sample rails app to run xmp.js on to test the extension from an external location. woo, hax.

---
+++ LocalTestServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalTestServer::Application.config.secret_token = '069b0c44007c7a7f7f474bad4e9a14c07d6d9901d30b6d9f633405cf0d1f4d9a49281b286c4f57677e37f02cce04649e7c3f8d7baa094309bea0fa409c9d15fd'
